7bb564bb18942df1a23332ae616c150cb836bc6f
stephanie <73750813+stephighodalo@users.noreply.github.com>
2023-09-28 21:11:22 +0100

    Add files via upload

---
+++ Dim_Calendar.sql
@@ -0,0 +1,13 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  [EnglishDayNameOfWeek] AS Day, 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,  
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --[CalendarSemester],  
+FROM 
+ [AdventureWorksDW2022].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2019
