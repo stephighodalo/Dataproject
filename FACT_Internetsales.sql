e263ef997fa0ee528d1a8ac31e0a93fb1c363367
stephanie <73750813+stephighodalo@users.noreply.github.com>
2023-09-28 21:13:01 +0100

    Add files via upload

---
+++ FACT_Internetsales.sql
@@ -0,0 +1,15 @@
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] 
+ 
+FROM 
+  [AdventureWorksDW2022].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Ensures we always only bring two years of date from extraction.
+ORDER BY
+  OrderDateKey ASC
